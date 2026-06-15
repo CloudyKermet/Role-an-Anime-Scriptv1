96857087505084bdb25e71745a26ea83d3051567
CloudyKermet <gamingbright2011@gmail.com>
2026-06-15 11:38:10 -0700

    Implement auto-collect cash feature
    Added auto-collect feature to claim cash periodically.

--- Main.lua
+++ Main.lua
@@ -1,1 +1,105 @@
 print("Loaded")
+local plr = game.Players.LocalPlayer
+local RunService = game:GetService("RunService")
+local autocollect = false
+
+RunService.RenderStepped:Connect(function(dt)
+   if autocollect then
+local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "1"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "2"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "3"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "4"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "5"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "6"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "7"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "8"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "9"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "10"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "11"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "12"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "13"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "14"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "15"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "16"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "17"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "18"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "19"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "20"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "21"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "22"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "23"
+)
+      local Event = game:GetService("ReplicatedStorage").Network.Client.ClaimCash
+Event:FireServer(
+    "24"
+)
+    end
+end)
